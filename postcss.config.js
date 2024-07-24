e581dab8a142ddd55eb2709621212c4f53afd8cc
catdonadieu <catdonadieu@gmail.com>
2024-07-24 21:26:59 +0200

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+import { default as pkg } from './package.json' assert { type: "json" };
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
+import banner from 'postcss-banner'
+const date = new Date();
+
+export default function (ctx) {
+	return {
+		map: ctx.options.map,
+		plugins: [
+			autoprefixer(),
+			cssnano(),
+			banner({
+				banner: `${pkg.name} — version ${pkg.version} — ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}
+	${pkg.description}
+	© 2014 — ${date.getFullYear()} ${pkg.author}`,
+				important: true
+			})
+		]
+	}
+}
---
+++ purgecss.docs.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+	content: ['docs/*.html'],
+	css: ['src/boosted/boosted.min.css'],
+	output: 'docs/css/boosted.min.css',
+	safelist: [],
+	variables: true,
+	fontFace: true,
+	keyframes: true
+}
